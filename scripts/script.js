8de234feb76a19fb78df837692079889b49e0394
Kevin Chuang <kcxinkun@gmail.com>
2016-03-03 20:33:57 -0800

    wrap file in IIFE

--- scripts/script.js
+++ scripts/script.js
@@ -1,45 +1,49 @@
-var articles = [];
-
-function Article (opts) {
-  this.title = opts.title;
-  this.category = opts.category;
-  this.author = opts.author;
-  this.authorUrl = opts.authorUrl;
-  this.publishedOn = opts.publishedOn;
-  this.body = opts.body;
-};
-
-Article.prototype.toHtml = function() {
-  var source = $('#article-template').html();
-  var template = Handlebars.compile(source);
-  var html = template(articles);
-  return template(this);
-};
-
-Article.loadAll = function(rawData) {
-  rawData.sort(function(a,b) {
-    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-  });
-};
-
-
-rawData.forEach(function(ele) {
-  articles.push(new Article(ele));
-});
-
-articles.forEach(function(a) {
-  $('#articles').append(a.toHtml());
-});
-
-articles.fetchArticles = function() {
-  if (localStorage.rawData) {
-    Article.loadAll(JSON.parse(localStorage.rawData));
-  }
-  else {
-    $.getJSON('data/blogArticles.json', function(rawData) {
-      Article.loadAll(rawData);
-      localStorage.rawData = JSON.stringify(rawData);
-      
+(function(module){
+  var scripts={};
+  var articles = [];
+  
+  function Article (opts) {
+    this.title = opts.title;
+    this.category = opts.category;
+    this.author = opts.author;
+    this.authorUrl = opts.authorUrl;
+    this.publishedOn = opts.publishedOn;
+    this.body = opts.body;
+  };
+  
+  Article.prototype.toHtml = function() {
+    var source = $('#article-template').html();
+    var template = Handlebars.compile(source);
+    var html = template(articles);
+    return template(this);
+  };
+  
+  Article.loadAll = function(rawData) {
+    rawData.sort(function(a,b) {
+      return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
     });
-  }
-};
+  };
+  
+  
+  rawData.forEach(function(ele) {
+    articles.push(new Article(ele));
+  });
+  
+  articles.forEach(function(a) {
+    $('#articles').append(a.toHtml());
+  });
+  
+  articles.fetchArticles = function() {
+    if (localStorage.rawData) {
+      Article.loadAll(JSON.parse(localStorage.rawData));
+    }
+    else {
+      $.getJSON('data/blogArticles.json', function(rawData) {
+        Article.loadAll(rawData);
+        localStorage.rawData = JSON.stringify(rawData);
+        
+      });
+    }
+  };
+  module.scripts = scripts;
+})(window);
